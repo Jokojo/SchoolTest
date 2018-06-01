fad15261e3d102af37e81f15c53e7363f3835149
Joey Kannegieter <200173@edu.rocfriesepoort.nl>
2018-06-01 14:27:26 +0200

    Alles
    Gem

---
+++ Poker/Assets/Scripts/WriteToDisk.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum WinState
+{
+    PLAYER1,
+    PLAYER2,
+    TIE
+}
+
+public class WriteToDisk : MonoBehaviour {
+    public static string[] output = new string[1000];
+
+    public static void WriteRoundToDisk()
+    {
+        System.IO.File.WriteAllLines(@"D:\output.txt", output);
+    }
+    
+    public static void AddOutput(int round, WinState state)
+    {
+        output[round] = "Round" + round + " " + state;
+    }
+}
